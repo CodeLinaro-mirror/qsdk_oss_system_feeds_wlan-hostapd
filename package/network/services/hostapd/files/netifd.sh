be80ffa21339290381afeed6360ba46ac81e4065
Aloka Dixit <alokad@codeaurora.org>
2020-10-04 14:15:27 -0700

    hostapd: Make the option hidden an integer
    Configuration option 'hidden' is a boolean used to set value for hostapd
    option 'ignore_boradcast_ssid' with integer value range 0 to 2.
    0 = Default, send complete SSID.
    1 = Empty SSID, length 0, included in beacons. Probe requests with
    broadcast SSID ignored.
    2 = Clear SSID (ASCII 0) with original length included in beacons. Probe
    requests with broadcast SSID ignored.
    
    This commit makes the option 'hidden' an integer to support all three
    values.
    
    Signed-off-by: Aloka Dixit <alokad@codeaurora.org>
    Change-Id: Id246c61d202683b5cddfe6bb6dd6fd58e5c37c08

--- package/network/services/hostapd/files/netifd.sh
+++ package/network/services/hostapd/files/netifd.sh
@@ -129,9 +129,9 @@ EOF
 
 hostapd_common_add_bss_config() {
 	config_add_string 'bssid:macaddr' 'ssid:string'
-	config_add_boolean wds wmm uapsd hidden
+	config_add_boolean wds wmm uapsd
 
-	config_add_int maxassoc max_inactivity
+	config_add_int maxassoc max_inactivity hidden
 	config_add_boolean disassoc_low_ack isolate short_preamble
 
 	config_add_int \
