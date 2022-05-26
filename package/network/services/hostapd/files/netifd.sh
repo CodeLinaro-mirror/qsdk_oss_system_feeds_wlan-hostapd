6d96edb5f63e185b0293469bb1cc6f12d867c055
Rameshkumar Sundaram <quic_ramess@quicinc.com>
2022-05-26 23:05:47 +0530

    wpa_supplicant: fix eht and he flags for 6GHz
    Set ieee80211ax and ieee80211be of interface config for
    6GHz band, based on he and eht capabilities.
    
    Send Proper eht mode capablities to hostapd_set_freq_params
    inorder to set eht related fielda in mesh freqeuncy parameters.
    
    Fix Puncturing bitmap configuration in wpa_supplicant.conf
    wpa_supplicant expects config string for Puncturing bitmap in the
    form of ru_punct_bitmap=2,
    whereas it is built as ru-puncturing-bitmap=2 (same as how iw uses)
    Fixing the same on hostapd script.
    
    Change-Id: I92d4c52ccff192c81a384466be935e2f33103f02
    Signed-off-by: Rameshkumar Sundaram <quic_ramess@quicinc.com>

--- package/network/services/hostapd/files/netifd.sh
+++ package/network/services/hostapd/files/netifd.sh
@@ -679,7 +679,7 @@ wpa_supplicant_add_network() {
 
 	local key_mgmt='NONE'
 	local enc_str=
-	local ru_punct_str=${ru_punct_bitmap:+ru-puncturing-bitmap=$ru_punct_bitmap}
+	local ru_punct_str=${ru_punct_bitmap:+ru_punct_bitmap=$ru_punct_bitmap}
 	local network_data=
 	local T="	"
 
