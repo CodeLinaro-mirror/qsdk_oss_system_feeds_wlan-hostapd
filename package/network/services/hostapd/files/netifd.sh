guoxiaoqiao/openwrt-qsdk

commit 430a1a9773ea89b86fc712d0f78219435a827fd1

hostapd: fix STA assoc in pmf enabled case
pmf enabled case expects auth type WPA-PSK-SHA256

Change-Id: Id287dff3ffb724f2adccefa389d74b71a8fe9f56
Signed-off-by: Anilkumar Kolli <akolli@codeaurora.org>

package/network/services/hostapd/files/netifd.sh
@@ -270,6 +270,7 @@ hostapd_set_bss_options() {
 		;;
 		psk)
 			json_get_vars key wpa_psk_file
+			json_get_var ieee80211w ieee80211w
 			if [ ${#key} -lt 8 ]; then
 				wireless_setup_vif_failed INVALID_WPA_PSK
 				return 1
@@ -283,7 +284,13 @@ hostapd_set_bss_options() {
 				append bss_conf "wpa_psk_file=$wpa_psk_file" "$N"
 			}
 			wps_possible=1
-			append wpa_key_mgmt "WPA-PSK"
+			if [ $ieee80211w -eq 2 ]; then
+				append wpa_key_mgmt "WPA-PSK-SHA256"
+			elif [ $ieee80211w -eq 1 ]; then
+				append wpa_key_mgmt "WPA-PSK WPA-PSK-SHA256"
+			else
+				append wpa_key_mgmt "WPA-PSK"
+			fi
 		;;
 		eap)
 			json_get_vars \
@@ -295,6 +302,7 @@ hostapd_set_bss_options() {
 				vlan_naming vlan_tagged_interface \
 				vlan_bridge
 
+			json_get_var ieee80211w ieee80211w
 			# legacy compatibility
 			[ -n "$auth_server" ] || json_get_var auth_server server
 			[ -n "$auth_port" ] || json_get_var auth_port port
@@ -327,7 +335,13 @@ hostapd_set_bss_options() {
 			[ -n "$ownip" ] && append bss_conf "own_ip_addr=$ownip" "$N"
 			append bss_conf "eapol_key_index_workaround=1" "$N"
 			append bss_conf "ieee8021x=1" "$N"
-			append wpa_key_mgmt "WPA-EAP"
+			if [ $ieee80211w -eq 2 ]; then
+				append wpa_key_mgmt "WPA-EAP-SHA256"
+			elif [ $ieee80211w -eq 1 ]; then
+				append wpa_key_mgmt "WPA-EAP WPA-EAP-SHA256"
+			else
+				append wpa_key_mgmt "WPA-EAP"
+			fi
 
 			[ -n "$dynamic_vlan" ] && {
 				append bss_conf "dynamic_vlan=$dynamic_vlan" "$N"

CodeLinaro-mirror/qsdk_oss_system_feeds_wlan-hostapd

commit 4371b26bb290420ad618ea4ae63f54b635e562f8

hostapd: fix pmf conf for WPA2-PSK based on the cert program
Below pasted the cert program
-----------
 The most common configuration options for this based on the PMF
 (protected
 management frames) certification program are:
 PMF enabled: ieee80211w=1 and wpa_key_mgmt=WPA-EAP WPA-EAP-SHA256
 PMF required: ieee80211w=2 and wpa_key_mgmt=WPA-EAP-SHA256
 (and similarly for WPA-PSK and WPA-PSK-SHA256 if WPA2-Personal is
 used)
 WPA3-Personal-only mode: ieee80211w=2 and wpa_key_mgmt=SAE
-----------

From the above statement if WPA2-Personal is used then WPA-PSK and
WPA-PSK-SHA256 both needs to be set for wpa_key_mgmt.

Fixes: 430a1a9773ea ("hostapd: fix STA assoc in pmf enabled case")

Change-Id: I13239693846ec91ebd3f7cce5edd87805e9c269a
Signed-off-by: Karthikeyan Kathirvel <quic_kathirve@quicinc.com>

package/network/services/hostapd/files/netifd.sh
@@ -288,9 +288,7 @@ hostapd_set_bss_options() {
 				append bss_conf "wpa_psk_file=$wpa_psk_file" "$N"
 			}
 			wps_possible=1
-			if [ $ieee80211w -eq 2 ]; then
-				append wpa_key_mgmt "WPA-PSK-SHA256"
-			elif [ $ieee80211w -eq 1 ]; then
+			if [ $ieee80211w -eq 2 ] || [ $ieee80211w -eq 1 ]; then
 				append wpa_key_mgmt "WPA-PSK WPA-PSK-SHA256"
 			else
 				append wpa_key_mgmt "WPA-PSK"